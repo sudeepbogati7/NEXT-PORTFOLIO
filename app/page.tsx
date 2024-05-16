cd18ee9dfbc514211bb4842645ec5d0bddae53e2
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-16 22:16:19 +0545

    Nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+'use client';
+import 'tailwindcss/tailwind.css';
 import Image from "next/image";
+import '@/app/theme.css';
+
+import Nav from '@/components/nav';
 
 export default function Home() {
   return (
-    <main className="">
-
-    </main>
+    <>
+      <header className='flex items-center justify-center'>
+        <Nav />
+        <div>
+          <label htmlFor="theme" className="theme">
+            <span className="theme__toggle-wrap">
+              <input id="theme" className="theme__toggle" type="checkbox" role="switch" name="theme" value="dark" />
+                <span className="theme__fill"></span>
+                <span className="theme__icon">
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                  <span className="theme__icon-part"></span>
+                </span>
+            </span>
+          </label>
+        </div>
+      </header>
+      <main className="mt-24">
+        Hey what is going on
+      </main>
+    </>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,16 +1,17 @@
 'use client';
-import 'tailwindcss/tailwind.css';
 import React from "react";
 
 export default function Nav() {
     return (
         <>
-            <nav>
+            <nav className='fixed top-0 p-4 font-medium my-2 text-white  rounded-2xl w-3/4 items-center mx-auto bg-[#0077b6] w-full flex gap-4 justify-around '>
                 <span> About </span>
                 <span> Contact  </span>
                 <span> Projects </span>
                 <span> Blogs </span>
+                
             </nav>
+            
         </>
     )
 }
