d8a94fe5356c588017635ec9c9a13de093296cef
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-16 21:26:24 +0545

    initial commit , project setup using Bun env

---
+++ components/nav.tsx
@@ -0,0 +1,16 @@
+'use client';
+import 'tailwindcss/tailwind.css';
+import React from "react";
+
+export default function Nav() {
+    return (
+        <>
+            <nav>
+                <span> About </span>
+                <span> Contact  </span>
+                <span> Projects </span>
+                <span> Blogs </span>
+            </nav>
+        </>
+    )
+}
