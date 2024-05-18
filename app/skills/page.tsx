7f4f3966f7818eb59ae130a9137044d5b7cbceba
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-18 23:21:50 +0545

    dynamic nav bar

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Skills() {
     return (
-        <div className="mt-24">
-            <h1>Skilss Section .... </h1>
-        </div>
+        <section className="mt-24">
+            <h1 className="font-semibold  text-xl px-4 ">What I do ?</h1>
+            <div></div>
+        </section>
     )
 }
