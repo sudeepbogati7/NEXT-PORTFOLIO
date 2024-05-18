d297b27e0c47be6fcc7c7147aa8d11d22a497900
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-18 16:26:02 +0545

    Logo , navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,32 @@
 import 'tailwindcss/tailwind.css';
 import Image from "next/image";
 import '@/app/theme.css';
-
+import Link from 'next/link';
 import Nav from '@/components/nav';
 
 export default function Home() {
   return (
     <>
-      <header className='flex items-center w-3/4  mx-auto justify-center'>
+      <header className=''>
         <Nav />
       </header>
       <main className="mt-24">
-        
+        <div className='flex flex-col  items-center justify-center '>
+          <div className=''>
+            <Image objectFit='cover' src={'/profile.jpg'} width={200} height={200} alt='profile-photo' className='rounded-full' />
+          </div>
+          <div className='flex flex-col py-4'>
+            <h1 className='font-semibold tracking-wide text-2xl  '> Sudeep Bogati</h1>
+            <div className='flex items-center text-base justify-center gap-1 '>
+            <span className='flex w-4 h-4'><Image src={'/location.png'} width={30} height={12} alt='location'></Image></span>
+            <span className='tracking-wide'>Chitwan, Nepal 🇳🇵</span>
+            </div>
+          </div>
+          <span>  </span>
+          <span> <Link target='_blank' href={'https://cct.tu.edu.np/courses/188'} className='hover:border-b-2 cursor-pointer border-gray-400 font-medium'> BIT </Link> Student at <span className='font-medium  '>Tribhuvan University </span></span>
+          <span>Associate Software Engineer at <Link target='_blank' href={'https://www.gtn.com.np'} className='font-semibold hover:border-b-2 border-gray-400 '>Greentick Nepal</Link></span>
+
+        </div>
       </main>
     </>
   );
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,17 +1,22 @@
 'use client';
 import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
 export default function Nav() {
     return (
         <>
-            <nav className='fixed top-0 p-4 font-medium my-2 text-white  rounded-2xl items-center mx-auto bg-[#0077b6] w-full flex gap-4 justify-around '>
-                <span> About </span>
-                <span> Contact  </span>
-                <span> Projects </span>
-                <span> Blogs </span>
-                
+            <nav className='fixed h-16 top-0 px-4  font-medium  w-full items-center mx-auto  flex gap-4 justify-between '>
+                <div className="p-2 "><Image objectFit="cover" src={'/logo-colored.png'} width={120} height={40} alt="logo"></Image></div>
+                <div className="flex gap-6  py-2 transition-all duration-200 ease-in-out">
+                    <span className="  border-b-2 border-gray-300"> Home </span>
+                    <span className=" hover:border-b-2 border-gray-300"> Skills </span>
+                    <span className=" hover:border-b-2 border-gray-300"> Projects </span>
+                    <span className=" hover:border-b-2 border-gray-300"> Blogs </span>
+                    <span className=" hover:border-b-2 border-gray-300"> Contact </span>
+                </div>
+
             </nav>
-            
+
         </>
     )
 }
