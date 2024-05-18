eaedca9491e780c0a875286d1d6d751673df8587
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-18 17:40:26 +0545

    Logo , navbar

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
                   d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
             </span>
-            <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer hover:text-green-800">
+            <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer hover:text-green-600">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -98,7 +98,6 @@ export default function Home() {
               </svg>
             </span>
           </div>
-
         </div>
       </main>
     </>
