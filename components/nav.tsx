d9510d116795c2a7732ac76ac2b2e4c1cbb07df3
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-29 13:01:45 +0545

    Bun setup,nav modifications

--- components/nav.tsx
+++ components/nav.tsx
@@ -11,7 +11,7 @@ export default function Nav() {
     };
     return (
         <>
-            <nav className='fixed h-16 top-0 px-4  w-full items-center mx-auto  flex gap-4 justify-between '>
+            <nav className='fixed bg-white z-50 border-b-2 border-gray-300 h-16 top-0 px-4  w-full items-center mx-auto  flex gap-4 justify-between '>
                 <Link href={'/'} className="p-2 "><Image objectFit="cover" src={'/logo-colored.png'} width={100} height={40} alt="logo"></Image></Link>
                 <div className="flex gap-6  py-2 transition-all duration-200 ease-in-out">
                     <span className={` ${isActive('/')}  `}> <Link href={'/'}>Home  </Link></span>
