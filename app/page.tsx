20b2393cd8e6911e041b3f2ee1550f43362da6c2
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-05 14:41:10 +0545

    alert message

--- app/page.tsx
+++ app/page.tsx
@@ -130,6 +130,13 @@ export default function Home() {
         </div> */}
 
           <Link href={'/about'} className='mt-14 border border-gray-400 px-2 rounded-lg text-gray-700 hover:border-black hover:text-black transition-all duration-200 ease-in-out bg-gray-100'>Know more about me </Link>
+          <div className='mt-8 font-medium text-blue-600 '> Site is under construction ......... </div>
+
+          <div className='flex flex-col mt-8'>
+            <span className='font-semibold '>Contact</span>
+            <span className='italic tracking-widest'>hello@sudipbogati.com.np</span>
+          </div>
+
         </div>
       </main>
     </>
