57c1804258b7653e53a4a277e7a960ddf0f29e0a
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-15 11:13:29 +0545

    Meta tags for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,21 @@ export default function RootLayout({
         <meta name="description" content="Hello , this is Sudeep Bogati , a passionate Software Engineer from Nepal. Explore my projects and skills in Full Stack Software Development." />
         <meta name="keywords" content="Sudip Bogati, Sudeep Bogati, sudeepbogati, sudipbogati , Software Engineer,  portfolio, skills, web developer, projects" />
         <link rel="canonical" href="https://sudipbogati.com.np" />
+
+
+        <meta property="og:title" content="Sudeep Bogati - Software Engineer" />
+        <meta property="og:description" content="Hello, this is Sudeep Bogati, a passionate Software Engineer from Nepal. Explore my projects and skills in Full Stack Software Development." />
+        <meta property="og:url" content="https://sudipbogati.com.np" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/profile.jpg" />
+
+
+
+        <meta name="twitter:card" content="summarsy_large_image" />
+        <meta name="twitter:title" content="Sudeep Bogati - Software Engineer" />
+        <meta name="twitter:description" content="Hello, this is Sudeep Bogati, a passionate Software Engineer from Nepal. Explore my projects and skills in Full Stack Software Development." />
+        <meta name="twitter:image" content="/profile.jpg" />
+
         <script type="application/ld+json">
           {`
           {
