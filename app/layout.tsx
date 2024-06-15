c4c8edafb5b143ce0d4fb33080799a06d61cae35
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-15 11:03:38 +0545

    Sitemap seo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Nav from "@/components/nav";
 import Loading from "./Loading";
 const inter = Inter({ subsets: ["latin"] });
 import { Suspense } from "react";
+
+import Head from "next/head";
 // export const metadata: Metadata = {
 //   title: "Sudeep Bogati",
 //   description: "Hello , I'm Sudeep Bogati . A passionate Software Engineer from Nepal",
@@ -25,7 +27,26 @@ export default function RootLayout({
         <meta name="description" content="Hello , this is Sudeep Bogati , a passionate Software Engineer from Nepal. Explore my projects and skills in Full Stack Software Development." />
         <meta name="keywords" content="Sudip Bogati, Sudeep Bogati, sudeepbogati, sudipbogati , Software Engineer,  portfolio, skills, web developer, projects" />
         <link rel="canonical" href="https://sudipbogati.com.np" />
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Sudeep Bogati",
+            "url": "https://sudipbogati.com.np",
+            "sameAs": [
+              "https://www.linkedin.com/in/sudeepbogati",
+              "https://github.com/sudeepbogati7"
+            ],
+            "jobTitle": "Software Engineer",
+            "worksFor": {
+              "@type": "Organization"
+            }
+          }
+          `}
+        </script>
       </head>
+
       <header className=''>
         <Nav />
       </header>
