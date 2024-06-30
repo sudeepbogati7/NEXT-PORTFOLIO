dded8ce8ba6c1e968c064dcba45b0dbe7090961d
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-30 16:25:41 +0545

    landing page font

--- app/page.tsx
+++ app/page.tsx
@@ -23,18 +23,18 @@ export default function Home() {
             <Image src={'/profile.jpg'} width={200} height={200} alt='profile-photo' placeholder="blur"    blurDataURL="/profile.jpg" priority className='shadow-2xl shadow-gray-800/60 rounded-full border-gray-500 border-4 ' />
           </div>
           {/* name and addres */}
-          <div className='flex flex-col my-6'>
+          <div className='flex flex-col my-4'>
             <h1 data-aos="zoom-in" className='font-extrabold tracking-wide text-2xl  '> Sudeep Bogati</h1>
             <div data-aos="zoom-in" className='flex items-center text-base justify-center gap-1 '>
-              <span data-aos="zoom-in" className='flex w-4 h-4'><Image src={'/location.png'} width={30} height={12} alt='location'></Image></span>
-              <span data-aos="zoom-in" className='tracking-wide'>Chitwan, Nepal 🇳🇵</span>
+              <span data-aos="zoom-in" className='flex w-4 h-4'><Image src={'/location.png'} width={20} height={20} alt='location'></Image></span>
+              <span data-aos="zoom-in" className='tracking-wide text-sm'>Chitwan, Nepal 🇳🇵</span>
             </div>
           </div>
 
           {/* Position and status */}
           <div className='flex flex-col items-center justify-center '>
-            <span className='' data-aos="zoom-in"> <Link target='_blank' href={'https://cct.tu.edu.np/courses/188'} className='tracking-wide border-b-2 cursor-pointer border-gray-300 hover:border-gray-400 font-medium'> BIT </Link> Student at <span className='font-medium  '>Tribhuvan University </span></span>
-            <span data-aos="zoom-in" className=''>Software Engineer at <Link target='_blank' href={'https://www.gtn.com.np'} className='font-medium border-b-2 hover:border-[#92a91a]  transition-all duration-200 ease-in-out border-gray-300 '>Greentick Nepal</Link></span>
+            <span className='text-sm' data-aos="zoom-in"> <Link target='_blank' href={'https://cct.tu.edu.np/courses/188'} className='tracking-wide  cursor-pointer underline font-medium'> BIT </Link> Student at <span className='font-medium  '>Tribhuvan University </span></span>
+            <span data-aos="zoom-in" className=' text-sm'>Software Engineer at <Link target='_blank' href={'https://www.gtn.com.np'} className='font-medium underline hover:text-[#92a91a]  transition-all duration-200 ease-in-out border-gray-300 '>Greentick Nepal</Link></span>
           </div>
 
           {/* links  */}
