4f14c730d195f44e014013a4823ca989f1215860
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-07-02 17:21:25 +0545

    Minor changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -152,10 +152,6 @@ export default function About() {
                             <Link data-aos="fade-up" href={'https://www.gtn.com.np'} className="underline flex tracking-wide text-sm items-center mb-2 font-medium text-gray-800 w-fit hover:border-gray-600"> Greentick Nepal- Gyaneshwor, Kathmandu </Link>
                             <time data-aos="fade-up" className="block italic mb-2 text-sm font-normal leading-none text-gray-500 "> 3 March , 2024 - 29 May, 2024 </time>
                             <p data-aos="fade-up" className="mb-4  font-normal text-gray-600 "> Dedicated Full Stack Developer Intern with experience in building web applications using React, Next.js, Tailwind CSS, TypeScript, Django Rest Framework, and Python. Skilled in creating user-friendly interfaces, connecting frontends to backends, and collaborating effectively with teams.</p>
-                            {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 "><svg className="w-3.5 h-3.5 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-                            <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
-                        </svg> Download ZIP</a> */}
                         </li>
                     </ol>
                 </div>
