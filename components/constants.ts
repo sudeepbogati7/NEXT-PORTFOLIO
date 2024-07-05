e1b041936ff73d140edf64d4d9af8715b50213cb
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-07-05 17:15:10 +0545

    Minor changes

--- components/constants.ts
+++ components/constants.ts
@@ -1,8 +1,5 @@
 import { ThemeInput } from './types';
-
 export const API_URL = 'https://github-contributions-api.jogruber.de/v4/';
-
-// GitHub theme
 export const DEFAULT_THEME: ThemeInput = {
   light: ['#ebedf0', '#49ab62', '#32c256', '#20d64e', '#00ff40'],
   dark: ['#161b22', '#19824d', '#0e9c4f', '#05b054', '#0cf075'],
